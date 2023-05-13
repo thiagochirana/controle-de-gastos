5b7cad0d2625425a7539f0fadf669b89c5e571e2
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-13 02:01:04 -0300

    [FEATURE] implementacao do Flyway, ferramenta de versão do banco

---
+++ src/main/java/br/com/controlegastos/persistencia/versionamento/FlywayImpl.java
@@ -0,0 +1,72 @@
+package br.com.controlegastos.persistencia.versionamento;
+
+import br.com.controlegastos.persistencia.propriedades.Config;
+import br.com.controlegastos.persistencia.propriedades.PropriedadeException;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class FlywayImpl {
+
+    private static Logger LOG = LoggerFactory.getLogger(FlywayImpl.class);
+
+    public static void start() throws Exception {
+        try{
+            StringBuilder banco = new StringBuilder();
+            banco.append(Config.obterPropriedade("persistencia.url"));
+            banco.append(Config.obterPropriedade("persistencia.banco"));
+            String usuario = Config.obterPropriedade("persistencia.usuario");
+            String senha = Config.obterPropriedade("persistencia.senha");
+
+            String tabelaBanco;
+            try {
+                tabelaBanco = Config.obterPropriedade("flyway.tabela.banco");
+                LOG.debug("Tabela que será de base para o flyway/versionamento: "+tabelaBanco);
+            } catch (PropriedadeException e) {
+                LOG.warn("Não encontrado o campo \"flyway.realizar.versionamento.no.restart\" no config.properties."+
+                        "Setarei a tabela para padrão \"schema_version\"");
+                tabelaBanco = "schema_version";
+            }
+
+            LOG.debug("Inicio implementacao Flyway.");
+            Flyway flyway = Flyway.configure()
+                    .dataSource(banco.toString(), usuario, senha)
+                    .locations("classpath:db/patches")
+                    .table(tabelaBanco)
+                    .load();
+
+            try{
+                boolean versionamentoAoReiniciar = Boolean.parseBoolean(Config.obterPropriedade("flyway.realizar.versionamento.no.restart"));
+                LOG.debug("Reaplicacao de patches ao reiniciar? "+versionamentoAoReiniciar);
+                if (versionamentoAoReiniciar){
+                    flyway.baseline();
+                    LOG.debug("Realizado a reaplicação de patches");
+                }
+            }catch (PropriedadeException pe){
+                LOG.warn("Não encontrado o campo \"flyway.realizar.versionamento.no.restart\" no config.properties. Logo a reaplicação de patches será FALSE");
+                LOG.error(pe.getCause().toString());
+            }
+
+            try{
+                boolean versionamentoAtivo = Boolean.parseBoolean(Config.obterPropriedade("flyway.versionamento"));
+                if (versionamentoAtivo) {
+                    flyway.migrate();
+                }
+            }catch (PropriedadeException pe){
+                LOG.warn("Não encontrado o campo \"flyway.versionamento\" no config.properties. Logo o versionamento será FALSE");
+                LOG.error(pe.getCause().toString());
+            }
+
+        }catch (PropriedadeException pe){
+            LOG.error("Houve um erro ao carregar propriedades no Flyway: "+pe);
+            throw new PropriedadeException("Houve um erro ao carregar propriedades no Flyway.", pe);
+        }catch (IOException io){
+            throw io;
+        }catch (FlywayException fe){
+            throw new FlywayException("Nao foi possivel realizar o versionamento Flyway.",fe);
+        }
+    }
+}
