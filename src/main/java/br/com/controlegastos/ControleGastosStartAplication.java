fe51666cb628fb02049c2be0cdeb94e5e0227b43
João Nicholas <96755607+othiagomacedo@users.noreply.github.com>
2023-06-05 18:17:36 -0300

    [IMPROVEMENT] Ativar execucao do Flyway

--- src/main/java/br/com/controlegastos/ControleGastosStartAplication.java
+++ src/main/java/br/com/controlegastos/ControleGastosStartAplication.java
@@ -1,6 +1,9 @@
 package br.com.controlegastos;
 
+import br.com.controlegastos.persistencia.versionamento.FlywayImpl;
+
 public class ControleGastosStartAplication {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+        FlywayImpl.start();
     }
 }
