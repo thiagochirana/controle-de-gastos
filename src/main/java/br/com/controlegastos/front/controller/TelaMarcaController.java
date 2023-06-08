9a40aa228b89240065634b573665d61450a3f749
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-06-08 13:39:21 -0300

    [FEATURE] Adicao e ajuste telas

---
+++ src/main/java/br/com/controlegastos/front/controller/TelaMarcaController.java
@@ -0,0 +1,7 @@
+package br.com.controlegastos.front.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TelaMarcaController {
+}
--- src/main/java/br/com/controlegastos/front/view/TelaMarca.java
+++ src/main/java/br/com/controlegastos/front/view/TelaMarca.java
@@ -7,17 +7,18 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TelaMarca extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(Stage stage) throws IOException  {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TelaMarca.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
