009a9ff4265b1692033fc69590418d188d8afaab
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-06-09 20:32:13 -0300

    [FEATURE] Adicao e ajuste telas

--- src/main/java/br/com/controlegastos/front/controller/TelaModalCadastroMarcaController.java
+++ src/main/java/br/com/controlegastos/front/controller/TelaModalCadastroMarcaController.java
@@ -1,14 +1,24 @@
 package br.com.controlegastos.front.controller;
 
+import br.com.controlegastos.controle.MarcaController;
+import br.com.controlegastos.entidades.records.DadosCadastroMarca;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class TelaModalCadastroMarcaController {
 
+    String nomeMarca;
+
+    String caminhoArquivo;
+
     @FXML
     private Button btnCloseModalMarca;
+
+    @FXML
+    private TextField TxFdNomeMarca;
 
     @FXML
     private Button btnFileChooserImgMarca;
@@ -19,9 +29,19 @@ public class TelaModalCadastroMarcaController {
     }
 
     @FXML
+    void obterDadosMarca(MouseEvent event) {
+        this.nomeMarca = TxFdNomeMarca.getText();
+    }
+
+    @FXML
     void fecharModal(MouseEvent event) {
         Stage stm = (Stage) btnCloseModalMarca.getScene().getWindow();
         stm.close();
+    }
+
+    public void salvarMarca() throws Exception {
+        MarcaController marca = new MarcaController();
+        String mensagem = marca.cadastrarMarca(new DadosCadastroMarca(nomeMarca, caminhoArquivo)).mensagem();
     }
 
 }
