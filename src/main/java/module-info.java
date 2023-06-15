00d31760e0aea1c92efd821e7d901cde950084c9
Rodrigo Dourado <rodrigodourado32@hotmail.com>
2023-06-14 21:02:04 -0300

    [FEATURE] Tela Home Swing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,8 @@ module br.com.controlegastos.controlegastosapp {
     requires org.slf4j;
     requires java.sql;
     requires org.flywaydb.core;
+    requires java.desktop;
+    requires java.logging;
 
     opens br.com.controlegastos.front.view to javafx.fxml;
     exports br.com.controlegastos.front.view;
