c97c9b049828d4530ebf91ee2f8eeb07471a3283
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-06-17 00:02:56 -0300

    [IMPROVEMENT] Ajuste na migracao da tabela Veiculo

--- src/main/resources/auxiliar/V4__CRIAR_VEICULO.sql
+++ src/main/resources/auxiliar/V4__CRIAR_VEICULO.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Veiculo (
   id_veiculo SERIAL PRIMARY KEY UNIQUE,
+  tem_placa BOOLEAN NOT NULL DEFAULT TRUE,
   placa VARCHAR(10) NOT NULL,
-  foto BYTEA,
   tipo_combustivel VARCHAR(30),
   quilometragem FLOAT,
   categoria_veiculo VARCHAR(60),
+  ativo BOOLEAN NOT NULL DEFAULT TRUE,
   modelo_id BIGINT REFERENCES Modelo(id_modelo),
   proprietario_id BIGINT REFERENCES Proprietario(id_proprietario)
 );
