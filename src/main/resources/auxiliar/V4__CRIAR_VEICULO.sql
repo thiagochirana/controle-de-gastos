9e4ed661294c976b8efed26cc459b0ab911ce345
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-06-18 03:50:05 -0300

    [IMPROVEMENT] Ajuste em query Categoria Gasto e Veiculo

--- src/main/resources/auxiliar/V4__CRIAR_VEICULO.sql
+++ src/main/resources/auxiliar/V4__CRIAR_VEICULO.sql
@@ -2,6 +2,7 @@ CREATE TABLE Veiculo (
   id_veiculo SERIAL PRIMARY KEY UNIQUE,
   tem_placa BOOLEAN NOT NULL DEFAULT TRUE,
   placa VARCHAR(10),
+  descricao_veiculo TEXT,
   tipo_combustivel VARCHAR(30),
   quilometragem FLOAT,
   categoria_veiculo VARCHAR(60),
--- src/main/resources/auxiliar/V5__CRIAR_CATEGORIAGASTOS.sql
+++ src/main/resources/auxiliar/V5__CRIAR_CATEGORIAGASTOS.sql
@@ -1,4 +1,5 @@
 CREATE TABLE Categoria_Gasto (
   id_categoria SERIAL PRIMARY KEY UNIQUE,
-  nome VARCHAR(100) NOT NULL
+  nome VARCHAR(100) NOT NULL,
+  descricao_categoria TEXT
 );
