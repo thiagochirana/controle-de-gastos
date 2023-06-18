2389ecdfc34990a7abb3f77cca93a9ad032fe1ce
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-06-18 01:22:05 -0300

    [FEATURE] Acrescimo Enum Tipo de Combustível

---
+++ src/main/java/br/com/controlegastos/entidades/enums/TipoCombustivel.java
@@ -0,0 +1,12 @@
+package br.com.controlegastos.entidades.enums;
+
+public enum TipoCombustivel {
+    GASOLINA,
+    ETANOL,
+    DIESEL,
+    FLEX,
+    ELETRICO,
+    HIBRIDO,
+    GNV,
+    SEM_COMBUSTIVEL
+}
